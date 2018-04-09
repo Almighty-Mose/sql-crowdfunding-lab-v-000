2b784035ffeed0a90d9b34f54fa0b13533215974
Branden Mosley <branden.mosley1337@gmail.com>
2018-04-09 18:21:59 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,13 +1,13 @@
-INSERT INTO 
-INSERT INTO 
-INSERT INTO 
-INSERT INTO 
-INSERT INTO 
-INSERT INTO 
-INSERT INTO 
-INSERT INTO 
-INSERT INTO 
-INSERT INTO 
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ()
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ()
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ()
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ()
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ()
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ()
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ()
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ()
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ()
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ()
 
 INSERT INTO
 INSERT INTO
